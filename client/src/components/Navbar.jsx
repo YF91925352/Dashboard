282b614a50449e0ca832accd8bb9ee8fff88c6a9
YF91925352 <zyf91925352@outlook.com>
2024-04-17 21:58:52 +0800

    finish the basic function of the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { AppBar, IconButton, InputBase, Toolbar } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import React from "react";
-import { useDispatch } from "react-redux";
+
 import { FlexBetween } from "./FlexBetween";
 import {
-  LightModeOutlined,
   DarkModeOutlined,
+  LightModeOutlined,
   Menu as MenuIcon,
   Search,
   SettingsOutlined,
-  ArrowDropDownOutlined,
 } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -23,9 +24,31 @@ const Navbar = () => {
           <IconButton onClick={() => console.log("open sidebar")}>
             <MenuIcon />
           </IconButton>
-          <FlexBetween backgroundColor></FlexBetween>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="10px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
         </FlexBetween>
         {/* Right Side */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
       </Toolbar>
     </AppBar>
   );
