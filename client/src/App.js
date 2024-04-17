5f82f64ab79da1443aa4e5f989d3b55c17024009
YF91925352 <zyf91925352@outlook.com>
2024-04-17 17:15:18 +0800

    use redux to control the theme settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { useMemo } from "react";
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
