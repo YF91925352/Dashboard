87c603aea441d6bf6af84f1d4442ed407110f03a
YF91925352 <zyf91925352@outlook.com>
2024-04-27 18:58:24 +0800

    create the geography page with nivo

---
+++ client/src/scenes/geography/index.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { useGetGeographyQuery } from "state/api";
+import Header from "components/Header";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoData } from "state/geoData";
+
+const Geography = () => {
+  const theme = useTheme();
+  const { data } = useGetGeographyQuery();
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="GEOGRAPHY" subtitle="Find where your users are located." />
+      <Box
+        mt="40px"
+        height="75vh"
+        border={`1px solid ${theme.palette.secondary[200]}`}
+        borderRadius="4px"
+      >
+        {data ? (
+          <ResponsiveChoropleth
+            data={data}
+            theme={{
+              axis: {
+                domain: {
+                  line: {
+                    stroke: theme.palette.secondary[200],
+                  },
+                },
+                legend: {
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+                ticks: {
+                  line: {
+                    stroke: theme.palette.secondary[200],
+                    strokeWidth: 1,
+                  },
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+              },
+              legends: {
+                text: {
+                  fill: theme.palette.secondary[200],
+                },
+              },
+              tooltip: {
+                container: {
+                  color: theme.palette.primary.main,
+                },
+              },
+            }}
+            colors="nivo"
+            features={geoData.features}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            domain={[0, 60]}
+            unknownColor="#666666"
+            label="properties.name"
+            valueFormat=".2s"
+            projectionType="mercator"
+            projectionScale={150}
+            enableGraticule={false}
+            graticuleLineColor="#f1e9e9"
+            graticuleLineWidth={0.5}
+            projectionTranslation={[0.45, 0.6]}
+            projectionRotation={[0, 0, 0]}
+            borderWidth={1.3}
+            isInteractive={true}
+            borderColor="#ffffff"
+            fillColor=""
+            legends={[
+              {
+                anchor: "bottom-left",
+                direction: "column",
+                justify: true,
+                translateX: 10,
+                translateY: -10,
+                itemsSpacing: 1,
+                itemWidth: 94,
+                itemHeight: 18,
+                itemDirection: "left-to-right",
+                itemTextColor: theme.palette.secondary[200],
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#ffffff",
+                      itemOpacity: 1.2,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        ) : (
+          <Typography color={theme.palette.secondary[400]} variant="h4">
+            Loading...
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default Geography;
--- server/routes/client.js
+++ server/routes/client.js
@@ -3,10 +3,12 @@ import {
   getProducts,
   getCustomers,
   getTransactions,
+  getGeography,
 } from "../controllers/client.js";
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 export default router;
